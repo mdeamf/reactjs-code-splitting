f3e09821aedb763fc491de97696b2b5b34f5431e
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-15 22:54:08 -0300

    feat: experimenting with import()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
 function App() {
+
+  import("./libs/doingStuff").then(doingStuff => {
+    let sum = doingStuff.addTwoNumbers(1, 2);
+    console.log('[doingStuff sum]', sum);
+
+    doingStuff.logText('[doingStuff logText]');
+  });
+
   return (
     <div className="App">
       <p>Testing!</p>
---
+++ src/libs/doingStuff.js
@@ -0,0 +1,7 @@
+export const addTwoNumbers = (n1, n2) => {
+  return n1 + n2;
+};
+
+export const logText = (text) => {
+  return console.log(text);
+};
