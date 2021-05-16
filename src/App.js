d41d5d4b36fd6151b12f0445a95d90919dcbcd8d
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-15 23:14:07 -0300

    feat: experimenting with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React, { Suspense } from 'react';
+
+const LazyComponent = React.lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('./components/LazyComponent'));
+    }, 1500);
+  });
+});
 
 function App() {
-
-  import("./libs/doingStuff").then(doingStuff => {
+  import('./libs/doingStuff').then((doingStuff) => {
     let sum = doingStuff.addTwoNumbers(1, 2);
     console.log('[doingStuff sum]', sum);
 
@@ -12,6 +20,10 @@ function App() {
   return (
     <div className="App">
       <p>Testing!</p>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <LazyComponent />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/LazyComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LazyComponent () {
+  return (
+    <p>This is my Lazy Component!</p>
+  )
+}
